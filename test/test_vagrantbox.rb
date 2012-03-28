58a8aea26ac5ae921f0420a7fe508da3f0b4fd05
Jon Proulx <jon@jonproulx.com>
2012-03-28 13:52:17 -0400

    fix myconfig (close file after write & call it) and clean up after ourselves

--- test/test_vagrantbox.rb
+++ test/test_vagrantbox.rb
@@ -6,7 +6,7 @@ context "Setting up new test box" do
     setup do
       uniquifier = Time.now.strftime("%s")
       @temp_name = "test_vagrantbox." + uniquifier 
-      @myworkdir    =  "/tmp/ps_utest_wordir." +  uniquifier 
+      @myworkdir    =  "/tmp/ps_utest_workdir." +  uniquifier 
       @myconf         =  "/tmp/ps_utest_conf."  + uniquifier  + ".yaml"
       # override defaults so we don't stomp active data with tests
       @test_config = {
@@ -17,14 +17,18 @@ context "Setting up new test box" do
         },
         :workdir              => @myworkdir,
       }
-      File.open(@myconf, "w").write(@test_config.to_yaml)
+      File.open(@myconf, "w") do |f|
+        f.write(@test_config.to_yaml)
+      end
     end
 
     teardown do
       File.delete(@myconf)
+      FileUtils.rm_rf @myworkdir
     end
 
     should "not raise an exception" do
+      assert_nothing_raised { mybox=PuppetShow::VagrantBox.configure(@myconf) }
       assert_nothing_raised { mybox=PuppetShow::VagrantBox.new(@temp_name) }
     end
   end
