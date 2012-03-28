6167e3a2e6dda4d9fa3e8cf0a994bceebd691844
Jon Proulx <jon@jonproulx.com>
2012-03-28 12:34:29 -0400

    add basic nothing raised test

---
+++ test/test_vagrantbox.rb
@@ -0,0 +1,32 @@
+require 'helper'
+require 'puppetshow/vagrantbox'
+
+class TestVagrantBox < Test::Unit::TestCase
+context "Setting up new test box" do 
+    setup do
+      uniquifier = Time.now.strftime("%s")
+      @temp_name = "test_vagrantbox." + uniquifier 
+      @myworkdir    =  "/tmp/ps_utest_wordir." +  uniquifier 
+      @myconf         =  "/tmp/ps_utest_conf."  + uniquifier  + ".yaml"
+      # override defaults so we don't stomp active data with tests
+      @test_config = {
+        :known_vagrants => {
+          @temp_name => {
+            :box_url => "http://rocky-mountain.csail.mit.edu/pub/vagrant/ubuntu-oneric64.box",
+          },
+        },
+        :workdir              => @myworkdir,
+      }
+      File.open(@myconf, "w").write(@test_config.to_yaml)
+    end
+
+    teardown do
+      File.delete(@myconf)
+    end
+
+    should "not raise an exception" do
+      assert_nothing_raised { mybox=PuppetShow::VagrantBox.new(@temp_name) }
+    end
+  end
+
+end
